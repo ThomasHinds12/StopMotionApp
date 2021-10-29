4a9505bb43094f4cfdcccf787f77da8f98c08471
thomas <lpb18137@uni.strath.ac.uk>
2021-10-29 11:53:13 +0100

    Camera permissions and camera preview use case set up

--- app/src/main/java/com/example/stopmotionapp/MainActivity.java
+++ app/src/main/java/com/example/stopmotionapp/MainActivity.java
@@ -1,14 +1,72 @@
 package com.example.stopmotionapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultCaller;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.concurrent.ExecutionException;
+
 public class MainActivity extends AppCompatActivity {
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActivityResultLauncher<String> requestPermissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {});
+        setUpCamera(requestPermissionLauncher);
     }
+
+
+    //checks the permissions for camera and then sets up the camera if the permission is granted
+    private void setUpCamera(ActivityResultLauncher<String> requestPermissionLauncher) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED){
+            //following code very closely based on https://developer.android.com/training/camerax/preview#java
+            cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+            cameraProviderFuture.addListener(() -> {
+                try {
+                    ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                    bindPreview(cameraProvider);
+                } catch (ExecutionException | InterruptedException e) {
+                    //this should never be reached
+                }
+            }, ContextCompat.getMainExecutor(this));
+        } else {
+            requestPermissionLauncher.launch(Manifest.permission.CAMERA);
+            setUpCamera(requestPermissionLauncher);
+        }
+    }
+
+
+    //following code very closely based on https://developer.android.com/training/camerax/preview#java
+    //binds the preview use case to the activity lifecycle
+    private void bindPreview(@NonNull ProcessCameraProvider cameraProvider){
+        Preview preview = new Preview.Builder().build();
+
+        CameraSelector cameraSelector = new CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build();
+
+        PreviewView previewView = (PreviewView) findViewById(R.id.previewView);
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+
+        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector, preview);
+    }
+
 }
