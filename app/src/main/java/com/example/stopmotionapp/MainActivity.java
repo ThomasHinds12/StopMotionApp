700af337108d73bb2b3ed4fcaaacc14964af0fdc
thomas <lpb18137@uni.strath.ac.uk>
2021-11-10 15:48:06 +0000

    Taking photos but not doing anything with them, have an activity vidio activity now with just a video view. Plan is to create video in main activity first and then get figure out how to display the video.

--- app/src/main/java/com/example/stopmotionapp/MainActivity.java
+++ app/src/main/java/com/example/stopmotionapp/MainActivity.java
@@ -1,35 +1,45 @@
 package com.example.stopmotionapp;
 
 import android.Manifest;
-import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.media.Image;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
 
-import androidx.activity.result.ActivityResultCaller;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.Camera;
 import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
+import androidx.camera.core.ImageProxy;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
-import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import androidx.lifecycle.LifecycleOwner;
 
 import com.google.common.util.concurrent.ListenableFuture;
 
+import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
     private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+    ImageCapture imageCapture;
+    Preview preview;
+    ArrayList<Image> images;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        images = new ArrayList<>();
 
         ActivityResultLauncher<String> requestPermissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {});
         setUpCamera(requestPermissionLauncher);
@@ -44,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
             cameraProviderFuture.addListener(() -> {
                 try {
                     ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
-                    bindPreview(cameraProvider);
+                    bindPreviewAndImageCapture(cameraProvider);
                 } catch (ExecutionException | InterruptedException e) {
                     //this should never be reached
                 }
@@ -56,17 +66,50 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    //following code very closely based on https://developer.android.com/training/camerax/preview#java
+    //following code based on code from https://developer.android.com/training/camerax/preview#java
     //binds the preview use case to the activity lifecycle
-    private void bindPreview(@NonNull ProcessCameraProvider cameraProvider){
-        Preview preview = new Preview.Builder().build();
+    private void bindPreviewAndImageCapture(@NonNull ProcessCameraProvider cameraProvider){
+        preview = new Preview.Builder().build();
 
         CameraSelector cameraSelector = new CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build();
 
         PreviewView previewView = (PreviewView) findViewById(R.id.previewView);
         preview.setSurfaceProvider(previewView.getSurfaceProvider());
 
-        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector, preview);
+        imageCapture = new ImageCapture.Builder().build();
+        Camera camera = cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture);
     }
 
+
+    //following code based on code from https://developer.android.com/training/camerax/take-photo
+    //method to take photo when the 'Take Photo' button is clicked
+    public void takePhoto(View view){
+        //ImageCapture.OutputFileOptions outputFileOptions = new ImageCapture.OutputFileOptions.Builder(new File(imageCount + ".jpg")).build();
+        imageCapture.takePicture(ContextCompat.getMainExecutor(this), new ImageCapture.OnImageCapturedCallback() {
+            public void onError(ImageCaptureException e){
+                Log.e("TakePhotoFail", "The photo did not save due to " + e.toString());
+            }
+
+            @Override
+            @androidx.camera.core.ExperimentalGetImage
+            public void onCaptureSuccess(@NonNull ImageProxy image) {
+                super.onCaptureSuccess(image);
+                Log.d("PhotoTaken", image.toString());
+
+                if (image.getImage() != null){
+                    images.add(image.getImage());
+                }
+                Log.d("ImageListSize", String.valueOf(images.size()));
+                Log.d("Image[0]", images.get(0).toString());
+                image.close();
+            }
+        });
+    }
+
+
+    public void createVideo(View view){
+        Intent intent = new Intent(this, VideoActivity.class);
+        intent.putExtra("images", images);
+        startActivity(intent);
+    }
 }
---
+++ app/src/main/java/com/example/stopmotionapp/VideoActivity.java
@@ -0,0 +1,22 @@
+package com.example.stopmotionapp;
+
+import android.content.Intent;
+import android.media.Image;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class VideoActivity extends AppCompatActivity {
+    ArrayList<Image> images;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video);
+        Intent intent = getIntent();
+
+
+    }
+}
